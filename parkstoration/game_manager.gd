d736d5be099a2a90199da92d12bc6ec2e3cfddb1
50002 <142967542+50002@users.noreply.github.com>
2026-05-19 15:59:39 +0300

    Update game_manager.gd

--- parkstoration/game_manager.gd
+++ parkstoration/game_manager.gd
@@ -1,3 +1,10 @@
 extends Node
 
 var TrasH_Left : int = 10
+
+var Tasks : Dictionary = {
+	"Pick up trash": false,
+	"Replace Invasive flowers with native ones": false,
+	"Fix the shelter": false,
+	"Fully unblock the forest path": false
+}
