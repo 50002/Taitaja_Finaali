93f9e32df4ec688f68c9607f16f55d74da74ea2b
50002 <142967542+50002@users.noreply.github.com>
2026-05-19 16:13:23 +0300

    asd

---
+++ parkstoration/savedata2.gd
@@ -0,0 +1,21 @@
+extends Node
+
+var config = ConfigFile.new()
+var BGM = 0
+
+
+func _ready() -> void:
+
+	update_volume()
+
+func update_volume():
+	for volume in config.get_sections():
+		BGM = config.get_value(volume, "BGM")
+		
+	var bgm_index= AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bgm_index, BGM)
+
+func save_volume():
+	config.set_value("Volume", "BGM", BGM)
+	
+	config.save("user://savedata.cfg")
