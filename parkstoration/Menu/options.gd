0423c91ef70c40a331c5fb7bd11447cdbda08d06
veeti-from <142967295+veeti-from@users.noreply.github.com>
2026-05-19 15:08:24 +0300

    some

---
+++ parkstoration/Menu/options.gd
@@ -0,0 +1,40 @@
+extends Control
+@onready var move: AudioStreamPlayer = $move
+@onready var pick: AudioStreamPlayer = $pick
+@onready var back_timer: Timer = $back_timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$ColorRect/Container/VBoxContainer/bgm.grab_focus()
+
+
+
+
+func _on_bgm_drag_ended(value_changed: bool) -> void:
+	pick.play()
+
+
+func _on_sfx_drag_ended(value_changed: bool) -> void:
+	pick.play()
+
+
+func _on_back_pressed() -> void:
+	pick.play()
+	back_timer.start()
+
+
+func _on_bgm_focus_exited() -> void:
+	move.play()
+
+
+func _on_sfx_focus_exited() -> void:
+	move.play()
+
+
+func _on_back_focus_exited() -> void:
+	move.play()
+
+
+func _on_back_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://Menu/Mainmenu.tscn")
